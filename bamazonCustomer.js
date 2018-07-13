0158c9df6994c47e79dbad7ee9f958aade38af84
Adriana Shulman <adriana33@mac.com>
2018-07-13 14:28:57 -0400

    update password

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,11 +1,12 @@
 mysql = require('mysql');
+inquirer = require('inquirer');
 
 function login() {
     return mysql.createConnection({
         host: 'localhost',
         port: 3306,
         user: 'root',
-        password: 'Bigbudzz19',
+        password: 'password',
         database: 'bamazon',
     });
 }
--- schema.sql
+++ schema.sql
@@ -12,13 +12,14 @@ price INTEGER (10) NOT NULL,
 stock_quantity INTEGER (100) NOT NULL
 );
 
-INSERT into Inventory(product_name, department_name, price, stock_quantity)values("shirt", "basic", "10", "50");
-INSERT into Inventory(product_name, department_name, price, stock_quantity)values("pants", "basic", "30", "75");
-INSERT into Inventory(product_name, department_name, price, stock_quantity)values("hoodie", "basic", "20", "100");
-INSERT into Inventory(product_name, department_name, price, stock_quantity)values("romper", "fancy", "25", "4");
-INSERT into Inventory(product_name, department_name, price, stock_quantity)values("dress", "fancy", "50", "5");
-
-    SELECT *
-    FROM products;
-
+INSERT into products(product_name, department_name, price, stock_quantity)values("shirt", "basic", "10", "50");
+INSERT into products(product_name, department_name, price, stock_quantity)values("pants", "basic", "30", "75");
+INSERT into products(product_name, department_name, price, stock_quantity)values("hoodie", "basic", "20", "100");
+INSERT into products(product_name, department_name, price, stock_quantity)values("romper", "fancy", "25", "4");
+INSERT into products(product_name, department_name, price, stock_quantity)values("dress", "fancy", "50", "5");
+INSERT into products(product_name, department_name, price, stock_quantity)values("jacket", "outdoor", "100", "50");
+INSERT into products(product_name, department_name, price, stock_quantity)values("mitts", "ourdoor", "5", "50");
+INSERT into products(product_name, department_name, price, stock_quantity)values("hat", "outdoor", "15", "40");
+INSERT into products(product_name, department_name, price, stock_quantity)values("scarf", "outdoor", "10", "50");
+INSERT into products(product_name, department_name, price, stock_quantity)values("socks", "outdoor", "2", "100");
 
